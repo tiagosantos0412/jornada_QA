1abd3a71a5c0bd1c58a76fa565a918254bd8cb81
Tiago <tiagosantos0412@gmail.com>
2025-04-29 15:17:25 -0300

    Variáveis e tipos de daods javascript

---
+++ Programação Focada para QA  com JS/script.js
@@ -0,0 +1,32 @@
+//Variáveis e tipos de dados
+//numéricas
+let x = 10
+let y = 15
+console.log(x + y)
+idade = 38
+
+//string
+let nome = 'Tiago Felipe'
+let sobrenome = 'dos Santos'
+//concatenação
+let nomeCompleto = nome + ' ' + sobrenome
+console.log(nomeCompleto)
+
+//Bolean
+let maiorDeIdade = idade > 18
+
+console.log(maiorDeIdade)
+
+//arrays ou lista de dados
+let frutas = ['Maçã', 'Banana', 'Laranja', 'Melão']
+console.log(frutas[0])
+
+//objetos
+let cadastro = {
+    nome: 'Tiago',
+    idade: 38,
+    estadoCivil: 'Casado',
+    documento: '42568745x'
+}
+
+console.log(cadastro)
